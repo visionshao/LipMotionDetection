67927607f407641ce281fc05e40fbcceff9b71af
AndrewZ421 <z1054276327@gmail.com>
2022-04-10 16:45:35 +0800

    add faces folder to Dlib

--- Dlib/dlib_train.py
+++ Dlib/dlib_train.py
@@ -2,8 +2,7 @@ import os
 import dlib
 
 current_path = os.getcwd()
-
-faces_path = current_path + '/examples/faces'
+faces_path = current_path + '/faces'
 
 options = dlib.shape_predictor_training_options()
 options.oversampling_amount = 300
@@ -14,6 +13,4 @@ options.be_verbose = True
 training_xml_path = os.path.join(faces_path, "training_with_face_landmarks.xml")
 dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)
 print("Training accuracy:{0}".format(dlib.test_shape_predictor(training_xml_path, "predictor.dat")))
-
 testing_xml_path = os.path.join(faces_path,"testing_with_face_landmarks.xml")
-print("Testing accuracy:{0}".format(dlib.test_shape_predictor(testing_xml_path, "predictor.dat")))
