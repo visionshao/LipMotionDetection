3d737b0a2dc7a2ea7ddd40b8648e231cc5d0ee23
AndrewZ421 <z1054276327@gmail.com>
2022-04-11 20:35:38 +0800

    change save model path

--- Dlib/dlib_train.py
+++ Dlib/dlib_train.py
@@ -11,6 +11,6 @@ options.tree_depth = 2
 options.be_verbose = True
 
 training_xml_path = os.path.join(faces_path, "training_with_face_landmarks.xml")
-dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)
-print("Training accuracy:{0}".format(dlib.test_shape_predictor(training_xml_path, "predictor.dat")))
+dlib.train_shape_predictor(training_xml_path, "model/predictor.dat", options)
+print("Training accuracy:{0}".format(dlib.test_shape_predictor(training_xml_path, "model/predictor.dat")))
 testing_xml_path = os.path.join(faces_path,"testing_with_face_landmarks.xml")
